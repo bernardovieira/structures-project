ebf5caad6a9a0ee4a21732bee9d78eef7aa6abdf
Unknown <bernardo.vieira@envision-energy.com>
2018-01-19 11:38:16 -0700

    Bug fix on calculation of semi-circle second moment of inertia

--- calculateSecMoaSemiCircle.m
+++ calculateSecMoaSemiCircle.m
@@ -1,5 +1,5 @@
 % Calculate second moment of inertia for a semi-circle about axis collinear with base
 
 function [Ixx, Iyy] = calculateSecMoaSemiCircle(r) 
-Ixx = pi * r^2 /8;
-Iyy = pi * r^2 /8;
+Ixx = pi * r^4 /8;
+Iyy = pi * r^4 /8;
