701fd83e4d39435999e7890b54b92d3f438632b1
Unknown <bernardo.vieira@envision-energy.com>
2018-01-19 11:39:47 -0700

    Modified the test script to include calculation using the submodule

--- Test_script_rectangle.m
+++ Test_script_rectangle.m
@@ -1,4 +1,10 @@
+clear all;
+close all;
+clc
+
 % Test script
+
+addpath(genpath(pwd));
 
 % Calculate second moment of inertia for a rectangle
 b = 5;
@@ -12,6 +18,25 @@ R2 = 4;
 [Ixx_2, Iyy_2] = calculateSecMoaAnnulus(r1, R2);
 
 % Calculate second moment of inertia for a semi circle
-r = 1.45;
+r_semiCircle = 1.45;
 
-[Ixx_3, Iyy_3] = calculateSecMoaSemiCircle(r);
+[Ixx_3, Iyy_3] = calculateSecMoaSemiCircle(r_semiCircle);
+
+% Define the semicircle
+x0 = 0;
+y0 = 0;
+n_pts = 1000;
+theta = [ 0:pi/n_pts:pi 0];
+
+x_semiCirc = r_semiCircle*cos(theta) + x0; 
+y_semiCirc = r_semiCircle*sin(theta) + y0; 
+% y_semiCirc = y0 + sqrt(r_semiCircle.^2 - (x_semiCirc-x0).^2);
+
+figure;
+plot(x_semiCirc,y_semiCirc,'-o')
+axis equal
+
+[Ixx_3b, Iyy_3b] = computeSecMoaPolygon(x_semiCirc, y_semiCirc);
+
+Ixx_3 - Ixx_3b
+Iyy_3 - Iyy_3b
